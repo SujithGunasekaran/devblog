5ac16cbf387723dcee235045ac447956daf9478e
SujithGunasekaran <mydevelopstar@gmail.com>
2021-06-03 21:30:31 +0530

    mongodb connection added

--- server/config/index.js
+++ server/config/index.js
@@ -7,7 +7,9 @@ const configData = {
     GOOGLE_CLIENTID: process.env.GOOGLE_CLIENTID,
     GOOGLE_CLIENTSECRET: process.env.GOOGLE_CLIENTSECRET,
     GITHUB_CLIENTID: process.env.GITHUB_CLIENTID,
-    GITHUB_CLIENTSECRET: process.env.GITHUB_CLIENTSECRET
+    GITHUB_CLIENTSECRET: process.env.GITHUB_CLIENTSECRET,
+    MONGO_URI: process.env.MONGO_URI,
+    SESSION_SECRET: process.env.SESSION_SECRET
 }
 
 module.exports = configData;
---
+++ server/mongodb/index.js
@@ -0,0 +1,32 @@
+const mongooes = require('mongoose');
+const config = require('../config');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+
+exports.connect = () => {
+
+    const { MONGO_URI } = config;
+
+    mongooes.connect(`${MONGO_URI}`,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+        }, () => {
+            console.log("Mongodb connected Successfully");
+        })
+
+}
+
+exports.initializeMongodbSession = () => {
+
+    const { MONGO_URI } = config;
+
+    const store = new MongoDBStore({
+        uri: MONGO_URI,
+        collection: 'devBlogSession'
+    });
+
+    return store;
+}
