09946ef9c958434cfe87d9380d3e0e527a76f22d
SujithGunasekaran <mydevelopstar@gmail.com>
2021-06-05 22:15:20 +0530

    client apollo action and query updated

--- apollo/apolloActions.js
+++ apollo/apolloActions.js
@@ -1,4 +1,17 @@
-import { useQuery } from '@apollo/client';
-import { SAMPLE } from './apolloQueries';
+import { useQuery, useLazyQuery } from '@apollo/client';
+import {
+    GET_USER_INFO,
+    USER_LOGOUT
+} from './apolloQueries';
 
-export const useSample = () => useQuery(SAMPLE);
+
+// auth actions start
+
+export const useGetUserInfo = () => useLazyQuery(GET_USER_INFO);
+
+export const useLogout = () => useQuery(USER_LOGOUT);
+
+
+// auth actions end
+
+
--- apollo/apolloQueries.js
+++ apollo/apolloQueries.js
@@ -1,7 +1,25 @@
 import { gql } from '@apollo/client';
 
-export const SAMPLE = gql`
-    query Hello {
-        hello
+// auth queries start
+
+
+export const GET_USER_INFO = gql`
+    query GetUser {
+        getUserInfo {
+            _id
+            userid
+            username
+            userprofile
+       }
+    }
+`;
+
+export const USER_LOGOUT = gql`
+    query Logout {
+        logout
     }
 `
+
+
+
+// auth queries end
