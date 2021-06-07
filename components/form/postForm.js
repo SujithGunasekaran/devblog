7edd5162d99ca30b9b55af515fd82ef4020560ad
SujithGunasekaran <mydevelopstar@gmail.com>
2021-06-07 22:09:21 +0530

    rendering markdown text

--- components/form/postForm.js
+++ components/form/postForm.js
@@ -1,17 +1,29 @@
 import ReactMarkdown from 'react-markdown';
-import matter from 'gray-matter';
 import gfm from 'remark-gfm';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 const PostForm = (props) => {
 
     const { showPreview, postForm, handleFormField } = props;
 
-    const { data, content } = matter(postForm?.content ?? '');
+    let customRenderers = {}
 
-    const markdownText = {
-        ...data,
-        content
-    }
+    if (showPreview) {
+        customRenderers = {
+            code(code) {
+                const { className, children } = code;
+                const language = className && className.split('-')[1];
+                return (
+                    <SyntaxHighlighter
+                        style={atomDark}
+                        language={language}
+                        children={children}
+                    />
+                );
+            }
+        }
+    };
 
     return (
         <form>
@@ -25,8 +37,8 @@ const PostForm = (props) => {
             />
             {
                 showPreview ?
-                    <div className="post_form_textarea">
-                        <ReactMarkdown remarkPlugins={[gfm]}>{markdownText?.content ?? ''}</ReactMarkdown>
+                    <div className={`post_form_textarea hide_border`}>
+                        <ReactMarkdown remarkPlugins={[gfm, { singleTilde: false }]} skipHtml={false} components={customRenderers} children={postForm?.content ?? ''} />
                     </div> :
                     <textarea
                         className="post_form_textarea"
