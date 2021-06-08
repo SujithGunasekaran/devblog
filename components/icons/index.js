bacbf299153db5db64fbaa16b9b6808313db0a15
SujithGunasekaran <mydevelopstar@gmail.com>
2021-06-08 18:55:19 +0530

    Icon component added

---
+++ components/icons/index.js
@@ -0,0 +1,28 @@
+
+
+export const EditSquareIcon = ({ cssClass }) => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className={`bi bi-pencil-square ${cssClass}`} viewBox="0 0 16 16">
+            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+            <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+        </svg>
+    )
+}
+
+export const EyeIcon = ({ cssClass }) => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className={`bi bi-eye-fill ${cssClass}`} viewBox="0 0 16 16">
+            <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
+            <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
+        </svg>
+    )
+}
+
+export const InfoCircleIcon = ({ cssClass }) => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className={`bi bi-info-circle-fill ${cssClass}`} viewBox="0 0 16 16">
+            <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
+        </svg>
+    )
+}
+
