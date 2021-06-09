9c6c11582577564c6bb99aeff0edeba625b8b1b2
SujithGunasekaran <mydevelopstar@gmail.com>
2021-06-09 22:19:47 +0530

    home page updated getPostList integrated in client

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 import HeadTag from '../components/HeadTag';
 import withApollo from '../hoc/withApollo';
+import dynamic from 'next/dynamic';
+import { getDataFromTree } from '@apollo/client/react/ssr';
+import { useGetAllPost } from '../apollo/apolloActions';
+
+const DevLeftPanel = dynamic(() => import('../components/DevInfoLeftPanel.js'), { loading: () => <div>Loading Panel...</div> });
+const PostCard = dynamic(() => import('../components/post/PostCard'), { loading: () => <div>Loading Post...</div> });
 
 const Home = () => {
+
+  const { data: post, loading, error } = useGetAllPost();
 
   return (
     <div>
@@ -11,13 +19,31 @@ const Home = () => {
       />
       <div className="container-fluid">
         <div className="row">
-          <div className="col-md-12">
-            Home Component
+          <div className="col-md-3">
+            <DevLeftPanel />
+          </div>
+          <div className="col-md-6">
+            <div className="home_middle_head_container">
+              <div className="home_middle_head_info">Posts</div>
+            </div>
+            {
+              post && post.getAllPost &&
+              post.getAllPost.postList.map((postInfo, index) => (
+                <PostCard
+                  key={index}
+                  postInfo={postInfo}
+                />
+              ))
+            }
+            {
+              loading && <div>Loading Posts...</div>
+            }
           </div>
         </div>
       </div>
+      { error && <div></div>}
     </div>
   )
 }
 
-export default withApollo(Home);
+export default withApollo(Home, { getDataFromTree });
