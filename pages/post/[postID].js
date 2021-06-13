1617311c01dc0b08fded32bfb6d83afbfb16ed5e
SujithGunasekaran <mydevelopstar@gmail.com>
2021-06-13 19:51:32 +0530

    postID page updated

--- pages/post/[postID].js
+++ pages/post/[postID].js
@@ -1,8 +1,12 @@
 import { useRouter } from 'next/router';
+import dynamic from 'next/dynamic';
 import HeaderTag from '../../components/HeadTag';
 import withApollo from '../../hoc/withApollo';
 import { getDataFromTree } from '@apollo/client/react/ssr';
 import { useGetPostById } from '../../apollo/apolloActions';
+
+const PostDisplay = dynamic(() => import('../../components/post/FullPostInfo'));
+const Reaction = dynamic(() => import('../../components/panel/leftPanel/PostLeftPanel'));
 
 const PostInfo = () => {
 
@@ -10,6 +14,8 @@ const PostInfo = () => {
     const { postID } = router.query;
 
     const { data, loading, error } = useGetPostById(postID);
+
+    // data.getPostById.postInfo
 
     return (
         <div>
@@ -19,16 +25,27 @@ const PostInfo = () => {
                 description={"Get knowledge from the amazing developer posts"}
                 keyword={"React.js, Javascript, devBlog, devBlog Heroku"}
             />
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-md-3">
-                        Left Panel
-                    </div>
-                    <div className="col-md-6">
-                        Post info
-                    </div>
-                    <div className="col-md-3">
-                        Post created user detail
+            <div className="post_id_main">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-1">
+                            <div className="post_id_left_container">
+                                <Reaction />
+                            </div>
+                        </div>
+                        <div className="col-md-8">
+                            <div className="post_id_middle_container">
+                                {
+                                    data && data.getPostById && data.getPostById.postInfo &&
+                                    <PostDisplay
+                                        postData={data.getPostById.postInfo}
+                                    />
+                                }
+                            </div>
+                        </div>
+                        <div className="col-md-3">
+                            Post created user detail
+                        </div>
                     </div>
                 </div>
             </div>
