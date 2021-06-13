135efb452f4178d4c75f7657589469d7e24e6e29
SujithGunasekaran <mydevelopstar@gmail.com>
2021-06-13 19:53:14 +0530

    react markdown component added

---
+++ components/markdowns/MarkdownRender.js
@@ -0,0 +1,32 @@
+import ReactMarkdown from 'react-markdown';
+import gfm from 'remark-gfm';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+
+const MarkdownRender = (props) => {
+
+    const { content } = props;
+
+    let customRenderers = {}
+
+    customRenderers = {
+        code(code) {
+            const { className, children } = code;
+            const language = className && className.split('-')[1];
+            return (
+                <SyntaxHighlighter
+                    style={atomDark}
+                    language={language}
+                    children={children}
+                />
+            );
+        }
+    }
+
+    return (
+        <ReactMarkdown remarkPlugins={[gfm, { singleTilde: false }]} skipHtml={false} components={customRenderers} children={content} />
+    )
+
+}
+
+export default MarkdownRender;
