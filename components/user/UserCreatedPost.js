3b4df242c2b9790c8fa766a20e928324aabcd363
SujithGunasekaran <mydevelopstar@gmail.com>
2021-06-19 21:54:18 +0530

    useSaved and userCreated post component initia commit

---
+++ components/user/UserCreatedPost.js
@@ -0,0 +1,14 @@
+import withApollo from '../../hoc/withApollo';
+
+
+const UserCreatedPost = () => {
+
+    return (
+        <div>User created Post component</div>
+    )
+
+}
+
+export default withApollo(UserCreatedPost);
+
+
---
+++ components/user/UserSavedPost.js
@@ -0,0 +1,14 @@
+import withApollo from '../../hoc/withApollo';
+
+
+const UserSavePost = () => {
+
+    return (
+        <div>User created Post component</div>
+    )
+
+}
+
+export default withApollo(UserSavePost);
+
+
